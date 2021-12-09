9878b11cc5e859862c18fe025d2685d04c0c3c27
xavier9909 <sm798207@gmail.com>
2021-12-09 23:48:45 +0530

    bnb

---
+++ authentbvication/authentication/src/controllers/product.controller.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const Product = require("../models/product.model");
+
+const authenticate = require("../middlewares/authenticate");
+const authorise = require("../middlewares/authorise");
+
+const router = express.Router();
+
+router.post(
+  "/",
+  authenticate,
+  authorise(["seller", "admin"]),
+  async (req, res) => {
+    try {
+      const user = req.user;
+
+      const product = await Product.create({
+        name: req.body.name,
+        price: req.body.price,
+        image_urls: ["www.google.com"],
+        user: user.user._id,
+      });
+
+      return res.status(201).json({ product });
+    } catch (e) {
+      return res.status(500).json({ status: "failed", message: e.message });
+    }
+  }
+);
+
+router.get("/", async (req, res) => {
+  const products = await Product.find().lean().exec();
+
+  return res.send(products);
+});
+
+module.exports = router;
---
+++ authentbvication/authentication/src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const { register, login } = require("./controllers/auth.controller");
+const productController = require("./controllers/product.controller");
+
+const passport = require("./configs/passport");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(passport.initialize());
+// app.use("/users", userController) // /register /login
+
+passport.serializeUser(function ({ user, token }, done) {
+  done(null, { user, token });
+});
+passport.deserializeUser(function (user, done) {
+  done(err, user);
+});
+
+app.post("/register", register);
+app.post("/login", login);
+
+app.use("/products", productController);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+  }),
+  function (req, res) {
+    return res.status(201).json({ user: req.user.user, token: req.user.token });
+  }
+);
+
+app.get("/auth/google/failure", function (req, res) {
+  return res.send("Something went wrong");
+});
+
+module.exports = app;
