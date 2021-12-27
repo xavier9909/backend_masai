dadd901364c3c1aeb1249900d06e235af5859eef
xavier9909 <sm798207@gmail.com>
2021-12-27 17:25:14 +0530

    webpack

--- unit  5/day  1/src/index.js
+++ unit  5/day  1/src/index.js
@@ -1,9 +1,15 @@
 import {add} from './calc'
 import("../style.css")
+import React from 'react'
+import ReactDOM from 'react-dom'
 console.log(add(5,4));
 
 
-const h1 = document.createElement("h1")
-h1.innerHTML = "hello world"
-h1.classList.add("red")
-document.getElementById("root").appendChild(h1)
+// const h1 = document.createElement("h1")
+// h1.innerHTML = "hello world"
+// h1.classList.add("red")
+// document.getElementById("root").appendChild(h1)
+ReactDOM.render(
+    React.createElement("h1",{className: "red"}, "hello weeeee"),
+ document.getElementById("root")
+)
