b706bac1d8657075cdeb8028feff863ddfc84b66
xavier9909 <sm798207@gmail.com>
2022-01-12 11:27:08 +0530

    Update form.jsx

--- unit  5/day 4/forms/src/components/form.jsx
+++ unit  5/day 4/forms/src/components/form.jsx
@@ -1,9 +1,28 @@
+import {useRef, useState } from "react"
+
 export const Form = ()=>{
-return <form >
-<input type="text" placeholder="kuch toh daal" />
+    const [form,setform] =useState (null)
+     const ref = useRef(null)
+    const hndle = (e)=>{
+        console.log(ref.current.files);
+        const {name,value}  =e.target
 
+        setform({
+            ...form,
+            [name]:value
+        })
 
+    }
+    console.log(form);
+    const handle = (e)=>{
+e.preventDefault()
 
+    }
+return <form  onSubmit={handle}>
+<input onChange={hndle} type="text" placeholder="kuch toh daal" name="uname"/>
+<input onChange={hndle} type="text" placeholder="idhar bhi daal" name="age" />
+<input ref={ref} type="file" onChange={hndle} />
+<input type="submit" />
 
 
 </form>
